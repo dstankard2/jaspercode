f3e04ee05a72801afb254e78622c9fe775f06575
Dave Stankard <dstankard2@gmail.com>
2019-03-03 10:17:03 -0500

    Initial code check in.

---
+++ dist/src/test/java/net/sf/jaspercode/test/JasperTest.java
@@ -0,0 +1,42 @@
+package net.sf.jaspercode.test;
+
+import java.io.File;
+import java.util.HashMap;
+
+import net.sf.jaspercode.engine.JasperAgent;
+
+public class JasperTest {
+
+	private static File[] libs = new File[] {
+			new File("C:\\git\\jasperSPE\\patterns\\target\\classes"),
+			new File("C:\\git\\jasperSPE\\javascript-support\\target\\classes"),
+			new File("C:\\git\\jasperSPE\\java-support\\target\\classes"),
+			new File("C:\\git\\jasperSPE\\javascript-support\\target\\classes")
+	};
+
+	public static void main(String args[]) {
+		try {
+			runTest();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected static void runTest() throws Exception {
+		JasperAgent agent = null;
+		HashMap<String,String> options = new HashMap<>();
+
+		options.put("once", "");
+		options.put("applicationDir", "/SomeLinuxFolder");
+		options.put("applicationDir", "c://SomeWindowsFolder");
+
+		options.put("singleAppMode", "");
+		options.put("outputDir","/SomeLinuxFolder");
+		options.put("outputDir","c:\\SomeWindowsFolder");
+
+		agent = new JasperAgent(libs,options);
+		agent.start();
+	}
+
+}
+
