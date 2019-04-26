01825348300c6db8272d2faf63b1ae3036602127
Dave Stankard <dstankard2@gmail.com>
2019-04-26 07:54:34 -0400

    * Add examples that till test agent mode better.  Get a sense of how application manager pieces should interact in order to maintain application metainfo properly.

---
+++ engine/src/main/java/net/sf/jaspercode/engine/processing/AddComponentFileEntry.java
@@ -0,0 +1,72 @@
+package net.sf.jaspercode.engine.processing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sf.jaspercode.api.plugin.ProcessorLogMessage;
+import net.sf.jaspercode.engine.application.ProcessingManager;
+import net.sf.jaspercode.engine.definitions.ApplicationFolderImpl;
+import net.sf.jaspercode.engine.definitions.ComponentFile;
+import net.sf.jaspercode.engine.exception.PreprocessingException;
+
+public class AddComponentFileEntry implements Processable {
+
+	private ComponentFile componentFile = null;
+	private ProcessingManager processingManager = null;
+
+	public AddComponentFileEntry(ComponentFile componentFile, ProcessingManager applicationManager) {
+		this.componentFile = componentFile;
+		this.processingManager = applicationManager;
+	}
+
+	@Override
+	public int compareTo(Processable o) {
+		return -1;
+	}
+
+	@Override
+	public int getPriority() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<ProcessorLogMessage> getMessages() {
+		return new ArrayList<>();
+	}
+
+	public void preprocess() throws PreprocessingException {
+	}
+
+	@Override
+	public boolean process() {
+		processingManager.loadComponentFile(componentFile);
+		return true;
+	}
+
+	@Override
+	public ApplicationFolderImpl getFolder() {
+		return componentFile.getFolder();
+	}
+
+	// There's nothing to commit
+	@Override
+	public boolean commitChanges() {
+		return true;
+	}
+	@Override
+	public void rollbackChanges() {
+		// no-op
+	}
+
+	@Override
+	public ProcessingState getState() {
+		return ProcessingState.COMPLETE;
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
+	
+}
---
+++ example1/src/main/java/net/sf/jaspercode/example1/subsystem/impl/UserSubsystemImpl.java
@@ -0,0 +1,13 @@
+package net.sf.jaspercode.example1.subsystem.impl;
+
+import net.sf.jaspercode.example1.subsystem.UserSubsystem;
+import net.sf.jaspercode.example1.obj.UserData;
+
+public class UserSubsystemImpl extends UserSubsystem {
+
+	public Boolean saveUserData(UserData userData) {
+		return false;
+	}
+
+}
+
